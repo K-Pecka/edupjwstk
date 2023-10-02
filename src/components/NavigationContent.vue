1a10536d5b603be75a90b9fbfa2d73d4b58007f4
Kacper <krgpecka@gmail.com>
2023-10-02 22:16:06 +0200

    02-10-23

---
+++ src/components/NavigationContent.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+      <h2>Nawigacja</h2>
+      <!-- Zawartość nawigacji tutaj -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'NavigationContent',
+  };
+  </script>
